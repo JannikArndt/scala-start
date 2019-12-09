1f8231f5f2a38e984c9dabc50f923750e71d8eef
Jannik Arndt <jannik@jannikarndt.de>
2019-12-09 22:41:26 +0100

    Fix tests

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,16 +1,13 @@
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
-import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
 
-class MainSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpecLike with Matchers with BeforeAndAfterAll {
+class MainSpec extends TestKit(ActorSystem()) with AnyFlatSpecLike with Matchers with BeforeAndAfterAll {
 
-  def this() = this(ActorSystem("TestSystem"))
-
-  override def afterAll(): Unit = {
+  override def afterAll(): Unit =
     shutdown(system)
-  }
 
-  "This test" should "do nothing" in {
-
-  }
+  "This test" should "do nothing" in {}
 }
